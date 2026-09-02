27a80138376366c89eb77b13b70bec49867c9a79
MaximoLadner <mamoladner@gmail.com>
2026-09-02 11:44:48 -0300

    Verificar variables de Firebase

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,12 +1,28 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+console.log("Firebase env check:", {
+  projectId: !!projectId,
+  clientEmail: !!clientEmail,
+  privateKey: !!privateKey,
+});
+
+if (!projectId || !clientEmail || !privateKey) {
+  throw new Error(
+    `Faltan variables Firebase: projectId=${!!projectId}, clientEmail=${!!clientEmail}, privateKey=${!!privateKey}`
+  );
+}
+
 if (!getApps().length) {
   initializeApp({
     credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      projectId,
+      clientEmail,
+      privateKey: privateKey.replace(/\\n/g, "\n"),
     }),
   });
 }
