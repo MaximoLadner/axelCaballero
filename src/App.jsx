66053b06e7e104e74e4a31f7e54250cfbe87e2ac
MaximoLadner <mamoladner@gmail.com>
2026-09-02 23:57:40 -0300

    deje app como antes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Sorteo from "./pages/Sorteo";
-import Terminos from "./pages/Terminos";
-import Privacidad from "./pages/Privacidad";
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Sorteo />} />
-        <Route path="/terminos" element={<Terminos />} />
-        <Route path="/privacidad" element={<Privacidad />} />
-      </Routes>
-    </BrowserRouter>
+    <Sorteo />
   );
 }
 
